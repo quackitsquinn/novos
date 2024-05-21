493844b184ce579d9a86620a08877f53fd281ac4
QuackitsQuinn <quinnweismiller@gmail.com>
2024-05-21 11:17:18 -0500

    Have kernel use linkerscript

---
+++ kernel/build.rs
@@ -0,0 +1,6 @@
+/// Tell rust-lld to use the linker script
+const LINK_SCRIPT_PATH: &str = "kernel/link.ld";
+fn main() {
+    println!("cargo:rustc-link-arg=-T{}", LINK_SCRIPT_PATH);
+    println!("cargo:rerun-if-changed={}", LINK_SCRIPT_PATH);
+}
