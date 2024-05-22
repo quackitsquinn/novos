02e4aba018809162fb3a1db37818becf1796afb8
QuackitsQuinn <quinnweismiller@gmail.com>
2024-05-21 21:15:25 -0500

    add bios_boot run target

---
+++ src/bin/bios_boot.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+pub fn main() {
+    let mut command = Command::new("qemu-system-x86_64")
+        .arg("-cdrom")
+        .arg("target/artifacts/novos.iso") // We don't use a bios specific iso because it supports both
+        .arg("-serial")
+        .arg("stdio")
+        .spawn()
+        .expect("Failed to start qemu-system-x86_64");
+    let _ = command
+        .wait()
+        .expect("qemu-system-x86_64 exited with an error");
+}
