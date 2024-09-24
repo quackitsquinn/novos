699f72e7aa38424c4c1505026f108e79e5c56fb6
QuackitsQuinn <quinnweismiller@gmail.com>
2024-09-24 10:13:20 -0500

    Hopefully fix weird bug with bios_boot run code

--- src/bin/bios_boot.rs
+++ src/bin/bios_boot.rs
@@ -1,9 +1,10 @@
 use std::{
     env,
     fs::{File, OpenOptions},
-    io::{BufRead, BufReader, Read, Write},
-    process::{Command, Stdio},
+    io::{stdout, BufRead, BufReader, BufWriter, Read, Write},
+    process::{Child, Command, Stdio},
     str::Lines,
+    sync::{Arc, Mutex},
     thread,
 };
 
@@ -32,24 +33,46 @@ pub fn main() {
     let stderr = command.stderr.take().expect("Failed to get stderr");
     thread::spawn(move || handle_stream(Box::new(stdout), "stdout", true));
     thread::spawn(move || handle_stream(Box::new(stderr), "stderr", false));
+    // This is bad. We can't use a Arc<Mutex<Child>> because `command.wait()` will block the thread, so we are stuck with this bad code.
+    let child_ptr = &mut command as *mut _ as usize;
+    ctrlc::set_handler(move || {
+        let command = unsafe { &mut *(child_ptr as *mut Child) };
+        command.kill().expect("Failed to kill qemu-system-x86_64");
+        let mut buf_wrs = FILE_WRITERS.lock().unwrap();
+        for buf_wr in buf_wrs.iter_mut() {
+            buf_wr.flush().expect("Failed to flush");
+        }
+        FILE_WRITERS.lock().unwrap().clear();
+    });
     command.wait().expect("qemu-system-x86_64 failed to run");
 }
 
 type Reader = Box<dyn Read>;
 
+static FILE_WRITERS: Mutex<Vec<BufWriter<File>>> = Mutex::new(Vec::new());
+
 fn handle_stream(stream: Reader, name: &'static str, print: bool) {
     let mut br = BufReader::new(stream);
-    let mut f = File::create(format!("{}.log", name)).expect("Failed to create log file");
+    let mut bw =
+        BufWriter::new(File::create(format!("{}.log", name)).expect("Failed to create log file"));
+
+    let mut buf_wrs = FILE_WRITERS.lock().unwrap();
+    buf_wrs.push(bw);
+
+    let index = buf_wrs.len() - 1;
+
+    drop(buf_wrs);
 
     let mut lines = br.lines();
     while let Some(line) = lines.next() {
-        let line = line.expect("Failed to get line");
-        f.write_all(line.as_bytes())
-            .expect("Failed to write to file");
-        f.write_all(b"\n").expect("Failed to write to file");
+        let mut line = line.expect("Failed to get line");
+        line.push('\n');
+        FILE_WRITERS.lock().unwrap()[index]
+            .write_all(line.as_bytes())
+            .expect("Failed to write");
         if print {
-            println!("{}", line);
+            print!("{}", line);
         }
     }
-    unreachable!();
+    // If we reach this point, the stream has ended. We don't need to flush because the ctrlc handler will flush all the buffers.
 }
