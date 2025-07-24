cf2f351f1a3f37accde7d7b81b34d94d64608ec4
QuackItsQuinn <quinnweismiller@gmail.com>
2025-07-24 14:27:58 -0500

    Add gdb scratchpad

--- krun/src/gdb/instance.rs
+++ krun/src/gdb/instance.rs
@@ -25,6 +25,7 @@ impl Gdb {
 
 fn get_gdb_scripts() -> Vec<String> {
     let mut scripts = vec![];
+    let mut found_scratchpad = false;
     for file in fs::read_dir("gdb_scripts").unwrap() {
         let file = file.unwrap();
         if file.file_type().unwrap().is_file()
@@ -32,7 +33,21 @@ fn get_gdb_scripts() -> Vec<String> {
         {
             scripts.push(file.path().to_str().unwrap().to_string());
         }
+        // Check for scratchpad.gdb (A .gitignore'd file for debugging)
+        if file.file_name() == "scratchpad.gdb" {
+            found_scratchpad = true;
+        }
     }
+
+    if !found_scratchpad {
+        println!(
+            "Creating scratchpad.gdb! This file is ignored by git, so you can use it for debugging without worrying about committing it."
+        );
+        fs::write("gdb_scripts/scratchpad.gdb", "").expect("Failed to create scratchpad.gdb");
+        // Don't bother adding it to the list, as it will be created empty.
+    }
+
+    scripts.sort();
     scripts
 }
 
