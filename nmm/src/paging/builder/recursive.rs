ee27b326979189685ddbb5ca3100fb5f7001e3a9
quackitsquinn <quinnweismiller@gmail.com>
2026-09-13 20:41:15 -0500

    Use the right function to clean table

--- nmm/src/paging/builder/recursive.rs
+++ nmm/src/paging/builder/recursive.rs
@@ -7,7 +7,7 @@ use crate::{
     paging::{
         Address, AddressExt, FragmentManager, FragmentSize, Frame, MemoryFragment, Page, PageTable,
         PageTableEntry, PageTableIndex, PhysAddr, Small,
-        accessor::cleanup_l4_range,
+        accessor::{cleanup_l3, cleanup_l4_range},
         asm::{self, AddressSpace},
         map::{DataAllocator, Flush, MemoryMapper, PhysLinear, SizedMemoryMapper, Unmapped},
         operation::ZeroMemory,
@@ -85,13 +85,8 @@ impl<'a> Drop for RecursiveAddressSpaceBuilder<'a> {
         let mut pmm = asm::physical_memory_manager();
         let mut mapper = active_as.mapper.lock_inner_mapper();
         unsafe {
-            cleanup_l4_range(
-                PageTableIndex::iter_range(arch::RECURSIVE_SLOT1..arch::RECURSIVE_SLOT1),
-                &mut *mapper,
-                &mut *pmm,
-                false,
-            )
-            .expect("RecursiveAddressSpaceBuilder::drop failed!");
+            cleanup_l3(arch::RECURSIVE_SLOT1, &mut *mapper, &mut *pmm, false)
+                .expect("failed to cleanup recursive address space");
         }
         let current_pml4 = mapper.root_table_mut();
         unsafe { current_pml4.set_entry(arch::RECURSIVE_SLOT1, PageTableEntry::empty()) };
